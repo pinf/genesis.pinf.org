c6510249798e356356144b955893528a670e485f
Christoph Dorn <christoph@christophdorn.com>
2015-04-07 21:51:39 -0700

    wip

---
+++ .pgs/vortex/bin/activate.js
@@ -0,0 +1,20 @@
+
+const PATH = require("path");
+
+
+var runtimeDescriptor = require(PATH.join(process.cwd(), "program.rt.json"));
+
+if (runtimeDescriptor["pgs-prompt"]) {
+
+	for (var serviceId in runtimeDescriptor["pgs-prompt"].services) {
+
+		var service = runtimeDescriptor["pgs-prompt"].services[serviceId];
+
+		for (var name in service.variables) {
+			if (typeof service.variables[name].value !== "undefined") {
+				process.stdout.write('export ' + name + '=' + service.variables[name].value + "\n");
+			}
+		}
+	}
+
+}
